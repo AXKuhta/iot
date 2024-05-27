b38516f7f89b76ac7f9cc80333f3bb8307b9ee23
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2024-05-27 16:08:23 +0800

    Power management, MiniZ, raw data topic, telemetry topic

--- visitor_counter/read_mqtt.py
+++ visitor_counter/read_mqtt.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as paho
 from time import sleep
-
+import zlib
 
 #def send_command(cmd):
 #	device.write(cmd.encode())
@@ -13,8 +13,22 @@ def handle_connect_fn(client, userdata, flags, rc):
 	print("Connected")
 
 def handle_message_fn(client, userdata, message):
-	cmd = message.payload.decode()
-	print("event:", cmd)
+	payload = message.payload
+	topic = message.topic
+
+	if topic.endswith("sensors"):
+		print("visitors:", payload.decode())
+	elif topic.endswith("rawdata"):
+		try:
+			rawdata = zlib.decompress(payload)
+			print("rawdata:\n", rawdata.decode())
+			print(len(payload), "bytes gzip;", len(rawdata), "bytes plaintext")
+		except Exception as e:
+			print("rawdata: malformed", len(payload))
+	elif topic.endswith("telemetry"):
+		print("telemetry:", payload.decode())
+	else:
+		assert 0
 
 #broker = "broker.hivemq.com"
 broker = "broker.emqx.io"
@@ -29,6 +43,8 @@ client.connect(broker, port)
 client.loop_start()
 print("Subscribing")
 client.subscribe("axkuhta/esp32_ld2450_eed5/sensors")
+client.subscribe("axkuhta/esp32_ld2450_eed5/rawdata")
+client.subscribe("axkuhta/esp32_ld2450_eed5/telemetry")
 input("Press enter to exit")
 client.disconnect()
 client.loop_stop()
